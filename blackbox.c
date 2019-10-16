4a61b038b9c2d0da8c5653dcef7e5088b5d3fb55
sunghyungi <tjdgusrlek@gmail.com>
2019-10-16 10:06:50 +0900

    shg

--- blackbox.c
+++ blackbox.c
@@ -2,22 +2,23 @@
 #include<wiringPi.h>
 #include<stdlib.h>
 
-#define viv 15
-#define ALARM 23
+#define VIV 15
+#define BUZZER 23
 
 int main(){
 	if (wiringPiSetup() == -1)
 	    	return 1;
-	pinMode(viv,INPUT);
+	pinMode(VIV,INPUT);		//진동감지센서 INPUT
+	pinMode(BUZZER, OUTPUT);	//부저 OUTPUT
 
 	while(1){
-		int a = digitalRead(viv);
+		int a = digitalRead(VIV);	// a == 0 진동o, a == 1 진동x
 
 		printf("%d\n",a);
-		digitalWrite(ALARM,0);
-		if(a==0){
-			digitalWrite(ALARM,1);
-			system("sh black.sh 1");
+		digitalWrite(BUZZER,0);			// 평소에는 BUZZER을 LOW시켜서 소리가 안남
+		if(a==0){				// 진동이 울리면 
+			digitalWrite(BUZZER,1);		// HIGH시켜서 소리가 나게 함
+			system("sh black.sh 1");	// 카메라촬영하는 쉘 스크립트 실행.
 		delay(100);
 		}
 		delay(100);
